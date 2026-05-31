f0b00af51b011c2cd1271486d5c9485048c0c7bc
theod <theodore.peray@utbm.fr>
2026-05-31 16:57:19 +0200

    Maj Théodore, main.c. Debut du jeu

--- main.c
+++ main.c
@@ -2,34 +2,52 @@
 #include "jeu.h"
 #include "sauvegarde.h"
 int main(void) {
+    int choix;              //Variable qui stocke le choix de l'utilisateur dans le menu// Variable qui stocke le choix de l'utilisateur dans le menu
     int taille=0;           //Déclaration de la variable comprenant la valeur de la largeur de la grille
     Partie *p;              //Déclaration du pointeur p vers la structure Partie
+    int quitter =0;
 
-
-    afficherMenuDemineur();
-
-
-
-
-
-
+    srand(time(0));  //Initialise au hasard pour placer les mines aléatoirement
 
     p = malloc(sizeof(Partie));         //Allocation dynamique de mémoire pour la structure Partie
     if (p == NULL) {            //Test de réussite de l'allocation
         printf("Erreur d'allocation mémoire\n");
         return 1;
     }
-    creer_tableau_vide(p, taille);
+    while (quitter == 0) {
+        afficherMenuDemineur();
 
+        printf("Votre choix : ");
+        scanf("%d", &choix);
 
+        switch (choix) {
+            case 1:
+                lancer_nouvelle_partie(p);
+                break;
 
+            case 2:
+                printf("\nReprendre une partie n'est pas encore disponible.\n");
 
+                /*
+                Partie a faire avec l'onglet sauvegarde.c.
+                Appeler une fonction qui charge une partie sauvegardee.
+                */
+                break;
 
+            case 3:
+                printf("\nVous avez quitte le jeu.\n");
+                quitter = 1;
+                break;
 
+            case 4:
+                afficher_regles();
+                break;
 
-
-
-
+            /*
+            Controle q'acquisition a faire ? si utiliateur écrit autre chose que 1 2 3 4
+            */
+        }
+    }
 
     free(p);                            //Libération de la mémoire à la fin du programme
     return 0;
