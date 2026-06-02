4fe8b61b9f666518283df43971f726bcb7048757
jules <jules.werner@utbm.fr>
2026-06-02 21:45:43 +0200

    MàJ Jules Werner, là les mecs j'ai charbonné xD

--- sauvegarde.c
+++ sauvegarde.c
@@ -12,7 +12,85 @@ void SAUV_initialisation(){
     }
     fclose(f);
 }
-void SAUV_sauvegarde(Partie p)
+
+void SAUV_charger_partie(Partie *p)
+{
+    char ligne[5][1024];        //Création d'un tableau de 5 lignes avec 1024 caractères par ligne,
+    // collectant le contenu du fichier de sauvegarde
+    char temporaire[1024];      //Création d'un tableau de stockage temporaire pour copier le contenu
+    // du fichier avant d'introduire dans le tableau ligne
+    int num_ligne = 0;          //Déclaration d'une variable entière indiquant le numéro de ligne sur laquelle on extrait les données
+    FILE *f = fopen("..\\sauvegarde.csv", "r"); //Ouverture du fichier sauvegarde.csv en mode lecture
+    if (f == NULL)   //Vérification de la réussite de l'ouverture du fichier
+    {
+        printf("Erreur : impossible d'ouvrir le fichier sauvegarde.csv\n");
+        exit(1);
+    }
+
+    while (fgets(temporaire, 1024, f) != NULL) // Stocke chaque ligne dans un tableau
+    {                                                             // pour ne garder que les 5 dernières.
+        strcpy(ligne[num_ligne % 5], temporaire);
+        num_ligne++;
+    }
+    fclose(f);
+    //Chargement des données sauvegardées dans la structure Partie
+    // Ligne 1 : paramètres généraux
+    sscanf(ligne[0], "%d,%d,%d,%d,%d,%d,%d",
+           &p->mode,
+           &p->difficulte,
+           &p->taille,
+           &p->vies,
+           &p->tour,
+           &p->tour_malus,
+           &p->temps);
+
+    // Ligne 2 : mines
+    SAUV_charger_coordonnees(p, ligne[1], "mine");
+
+    // Ligne 3 : cases visibles
+    SAUV_charger_coordonnees(p, ligne[2], "visible");
+
+    // Ligne 4 : malus
+    SAUV_charger_coordonnees(p, ligne[3], "malus");
+
+    // Ligne 5 : bonus
+    SAUV_charger_coordonnees(p, ligne[4], "bonus");
+}
+
+void SAUV_charger_coordonnees(Partie *p, const char *ligne, const char *champ)
+{
+    char temporaire[1024];
+    strcpy(temporaire, ligne);
+
+    char *couple = strtok(temporaire, ",");
+
+    while (couple != NULL)
+    {
+        char *x_str = strtok(couple, "-");
+        char *y_str = strtok(NULL, "-");
+
+        if (x_str && y_str!=NULL)
+        {
+            int x = atoi(x_str);
+            int y = atoi(y_str);
+
+            if (strcmp(champ, "mine") == 0)
+                p->grille[x][y].mine = 1;
+
+            else if (strcmp(champ, "visible") == 0)
+                p->grille[x][y].visible = 1;
+
+            else if (strcmp(champ, "bonus") == 0)
+                p->grille[x][y].bonus = 1;
+
+            else if (strcmp(champ, "malus") == 0)
+                p->grille[x][y].malus = 1;
+        }
+
+        couple = strtok(NULL, ",");
+    }
+}
+void SAUV_sauvegarde(Partie *p)
 {
     FILE *f = fopen("..\\sauvegarde.csv", "a");
     //Ouverture du fichier sauvegarde.csv en mode sur écriture additionnelle
@@ -24,60 +102,47 @@ void SAUV_sauvegarde(Partie p)
     }
 
 
-
-    fprintf(f,"%d,%d,%d,",p.mode,p.difficulte,p.taille);
-    //Sauvegarde du mode de jeu, de la difficulté de la partie et de la taille de la grille
-
-    fprintf(f,"\n");        //Retour à la ligne pour sauvegarde des prochaines informations
-
-    ecrire_coordonnees(p,f,"mine");       //Sauvegarde des positions des mines de la forme x-y
+    fprintf(f,"%d,%d,%d,%d,%d,%d,%d",p->mode,p->difficulte,p->taille,p->vies,p->tour,p->tour_malus,p->temps);
+    //Sauvegarde du mode de jeu, de la difficulté de la partie, de la taille de la grille,
+    //du nombre de vies, du numéro du tour actuel, celui d'activation du dernier malus et du temps mis précédemment
 
     fprintf(f,"\n");        //Retour à la ligne pour sauvegarde des prochaines informations
 
-    ecrire_coordonnees(p,f,"taille");          //Sauvegarde des positions des cases relevées de la forme x-y
-
+    ecrire_coordonnees(p,f,"mine");           //Sauvegarde des positions des mines de la forme x-y
+    ecrire_coordonnees(p,f,"taille");         //Sauvegarde des positions des cases relevées de la forme x-y
+    ecrire_coordonnees(p,f,"malus");          //Sauvegarde des positions des cases malus de la forme x-y
+    ecrire_coordonnees(p,f,"bonus");          //Sauvegarde des positions des cases bonus de la forme x-y
 
 
     fclose(f);
 }
-void SAUV_charger_partie(Partie p)
+void ecrire_coordonnees(Partie *p, FILE *f, const char *champ)
 {
-    FILE *f = fopen("..\\sauvegarde.csv", "r");
-    if (f == NULL)
+    for (int i = 0; i < p->taille; i++)
     {
-        printf("Erreur : impossible d'ouvrir le fichier sauvegarde.csv\n");
-        exit(1);
-    }
-    fscanf(f,"%d",&p.mode);
-    fclose(f);
-}
-
-void ecrire_coordonnees(Partie p, FILE *f, const char *champ)
-{
-    for (int i = 0; i < p.taille; i++)
-    {
-        for (int j = 0; j < p.taille; j++)
+        for (int j = 0; j < p->taille; j++)
         {
             int valeur = 0; //Définition d'une variable qui vaut 1 si la case de coordonnées (i,j)
                             // du paramètre "champ" concerné vaut 1 et 0 sinon
             if (strcmp(champ, "visible") == 0)  //Recherche du paramètre à modifier en fonction de la chaîne "champ"
                 {
-                valeur = p.grille[i][j].visible;
+                valeur = p->grille[i][j].visible;
                 }
             else if (strcmp(champ, "mine") == 0)
                 {
-                valeur = p.grille[i][j].mine;
+                valeur = p->grille[i][j].mine;
                 }
             else if (strcmp(champ, "bonus") == 0)
                 {
-                valeur = p.grille[i][j].bonus;
+                valeur = p->grille[i][j].bonus;
                 }
             else if (strcmp(champ, "malus") == 0)
                 {
-                valeur = p.grille[i][j].malus;
+                valeur = p->grille[i][j].malus;
                 }
             if (valeur == 1)
                 fprintf(f, "%d-%d,", i, j);
         }
     }
+    fprintf(f,"\n"); //Retour à la ligne pour sauvegarde des prochaines informations
 }
--- sauvegarde.h
+++ sauvegarde.h
@@ -11,11 +11,33 @@
  * @return Rien.
  */
 void SAUV_initialisation();
+/**
+ * @brief Charge la dernière partie sauvegardée en lisant directement les 5 dernières lignes du fichier.
+ *
+ * La fonction ouvre le fichier sauvegarde.csv et lit toutes ses lignes.
+ * Seules les 5 dernières sont conservées, car elles correspondent à la dernière partie sauvegardée.
+ * La première ligne contient les paramètres généraux de la partie.
+ * Les quatre suivantes contiennent les coordonnées des mines,des cases visibles, malus et bonus.
+ *
+ * @param p Pointeur vers la structure Partie à remplir.
+ * @return Rien.
+ */
+void SAUV_charger_partie(Partie *p);
+/**
+ * @brief Charge des coordonnées de la forme x-y dans la grille.
+ *
+ * La fonction reçoit une ligne contenant des couples x-y séparés par des virgules.
+ * Chaque couple correspond à une case de la grille dont la valeur doit être activée.
+ *
+ * @param p Pointeur vers la structure Partie.
+ * @param ligne Chaîne contenant les coordonnées à charger.
+ * @param champ Chaîne indiquant le champ à modifier (mine, visible, bonus ou malus).
+ * @return Rien.
+ */
+void SAUV_charger_coordonnees(Partie *p, const char *ligne, const char *champ);
 
-void SAUV_sauvegarde(Partie p);
 
-void SAUV_charger_partie(Partie p);
-
-void ecrire_coordonnees(Partie p, FILE *f, const char *champ);
+void SAUV_sauvegarde(Partie *p);
+void ecrire_coordonnees(Partie *p, FILE *f, const char *champ);
 #endif //PROJET_DEMINEUR_RIOS_WERNER_PERAY_SAUVEGARDE_H
 
