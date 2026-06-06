f45205784230b6523a56c6bec51d7c84d35fe1a3
theod <theodore.peray@utbm.fr>
2026-06-06 17:24:09 +0200

    Maj main.c deroulement partie

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "jeu.h"
 #include "sauvegarde.h"
+#include <stdlib.h>
+
+void jouer_partie(Partie *p);;
 
 int main(void) {
-    int choix;              //Variable qui stocke le choix de l'utilisateur dans le menu// Variable qui stocke le choix de l'utilisateur dans le menu
-    int taille=0;           //Déclaration de la variable comprenant la valeur de la largeur de la grille
-    Partie *p;              //Déclaration du pointeur p vers la structure Partie 
-    int quitter =0;
+    int choix;          // Choix de l'utilisateur dans le menu
+    Partie *p;          // Pointeur vers la structure Partie
+    int quitter = 0;    // 0 = on reste dans le programme, 1 = on quitte
 
     srand(time(0));  //Initialise au hasard pour placer les mines aléatoirement
 
@@ -25,31 +27,29 @@ int main(void) {
         switch (choix) {
             case 1:
                 lancer_nouvelle_partie(p);
-                p->tour=1;
-                while ( p->vies >0) // Compte des tours tant que le joueur a encore des vies
-                {
-                                            //POUR PLUS TARD : mettre gestion_malus avant affichage_grille
-                    p->tour++;
-                }
+                p->tour = 1;
+                jouer_partie(p);
                 break;
 
             case 2:
-                printf("\nReprendre une partie n'est pas encore disponible.\n");
-
-
-                //Partie a faire avec l'onglet sauvegarde.c.
-                //Appeler une fonction qui charge une partie sauvegardee.
-
+                if (SAUV_charger_partie(p) == 1) {
+                    printf("\nPartie chargee avec succes.\n");
+                    jouer_partie(p);
+                } else {
+                    printf("\nAucune sauvegarde disponible.\n");
+                }
                 break;
 
             case 3:
                 printf("\nVous avez quitte le jeu.\n");
+                printf("A bientot !\n");
                 quitter = 1;
                 break;
 
             case 4:
                 afficher_regles();
                 break;
+
             default:                                    //Message d'erreur si la saisie de l'utilisateur n'est pas valide
                 printf("Erreur de saisie ");
                 break;
@@ -59,4 +59,66 @@ int main(void) {
 
     free(p);                      //Libération de la mémoire à la fin du programme
     return 0;
+}
+
+/* Fonction qui gere une partie en cours */
+void jouer_partie(Partie *p) {
+    int action;
+    int ligne;
+    int colonne;
+
+    while (p->vies > 0 && p->etat == 0) {  // Tant que le joueur a encore au moins une vie et que la partie est encore en cours, on continue la boucle de jeu
+
+        gestion_malus(p);
+        afficher_grille(p);
+
+        printf("Vies restantes : %d\n", p->vies);
+        printf("Tour : %d\n", p->tour);
+
+        printf("\n1) Jouer un coup\n");
+        printf("2) Sauvegarder et revenir au menu\n");
+        printf("3) Revenir au menu sans sauvegarder\n");
+        printf("Votre choix : ");
+        scanf("%d", &action);
+
+        switch (action) {
+
+            case 1:
+                printf("Choisissez une ligne : ");
+                scanf("%d", &ligne);
+
+                printf("Choisissez une colonne : ");
+                scanf("%d", &colonne);
+
+                reveler_case(p, ligne, colonne);  // Appelle la fonction qui révèle la case choisie par le joueur avec les coordonnées ligne / colonne saisies
+
+                if (p->vies <= 0) {
+                    p->etat = 2;
+                    printf("\nVous avez perdu la partie.\n");
+                    return;
+                }
+
+                if (partie_gagnee(p) == 1) {
+                    p->etat = 1;
+                    printf("\nBravo ! Vous avez gagne la partie.\n");
+                    return;
+                }
+
+                p->tour++;
+                break;
+
+            case 2:
+                SAUV_sauvegarde(p);
+                printf("\nPartie sauvegardee. Retour au menu.\n");
+                return;
+
+            case 3:
+                printf("\nRetour au menu sans sauvegarder.\n");
+                return;
+
+            default:
+                printf("\nChoix invalide.\n");
+                break;
+        }
+    }
 }
